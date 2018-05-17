679627be913f814c9d35bbe15f7698d71ea4cf57
seljydef <seljydef@gmail.com>
2018-05-17 13:51:39 +0000

    fix.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp ./vimrc ~/.vimrc
 if [ ! -e ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -8,5 +9,7 @@ fi
 mkdir -p /home/vagrant/.vim/colors
 vim +'PlugInstall --sync' +qa
 
-curl https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
-curl https://github.com/git/git/blob/master/contrib/completion/git-completion.bash > ~/.git-compeletion.bash
+cp ./tmux.conf ~/.tmux.conf
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-compeletion.bash
