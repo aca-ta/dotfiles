7afe19615cb69b888b002136fdf46d00f501b10d
seljydef <seljydef@gmail.com>
2019-02-06 00:34:42 +0900

    update: PIPENV_VENV_IN_PROJECT=1

--- etc/shrc.sh
+++ etc/shrc.sh
@@ -45,6 +45,7 @@ export TERM=xterm-256color
 if [[ -s ~/.pyenv ]]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
+  export PIPENV_VENV_IN_PROJECT=1
   pyenv() {
     unset -f pyenv
     eval "$(pyenv init -)"
