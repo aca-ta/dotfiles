dbee70a3ed55f4fc573539be7085160af84b20e0
seljydef <seljydef@gmail.com>
2019-02-06 00:36:13 +0900

    add: python and YCM

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ sudo apt install zsh tmux
 
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+
+# pyenv
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+
+sudo apt install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev
+
+CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
+
+# YouCompleteMe
+sudo apt install -y build-essential cmake
