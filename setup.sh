3f4f2fdf1a4a13b811360a364f66329688e33f7c
seljydef <seljydef@gmail.com>
2019-02-06 12:40:00 +0900

    fix: add apt update & upgrade

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade
+
 sudo apt install zsh tmux
 
 # zplug
@@ -11,6 +13,8 @@ git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 sudo apt install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev
 
 CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
+pyenv global 3.7.0
+pyenv rehash
 
 # YouCompleteMe
 sudo apt install -y build-essential cmake
