2b6d71a5f360bddc88a610a295c437afb4b9fb25
seljydef <seljydef@gmail.com>
2019-02-07 22:28:35 +0900

    refactor.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+install_python(){
+  git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+
+  sudo apt install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev
+
+  CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
+  pyenv global 3.7.0
+  pyenv rehash
+  pip install pipenv pylint mypy
+}
+
+install_youcompleteme(){
+  sudo apt install -y build-essential cmake
+  ~/.vim/plugged/YouCompleteMe/install.py
+}
+
 sudo apt update && sudo apt upgrade
-
-sudo apt install zsh tmux
-
-# zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-
-# pyenv
-git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-
-sudo apt install -y git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev
-
-CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.0
-pyenv global 3.7.0
-pyenv rehash
-
-# YouCompleteMe
-sudo apt install -y build-essential cmake
-~/.vim/plugged/YouCompleteMe/install.py
-
-# tig
-sudo apt install -y tig
+sudo apt install -u zsh tmux tig
+install_python
+install_youcompleteme
