55970629291d5bb9ac3f08cbdbe7c1f47fb47017
aca-ta <seljydef@gmail.com>
2020-04-19 13:13:29 +0900

    update: add fd

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ install_tools(){
   case "${OSTYPE}" in
     darwin*)
       brew update
-      brew install zsh tmux reattach-to-user-namespace tig bat fzf coreutils gnu-tar
+      brew install zsh tmux reattach-to-user-namespace tig bat fzf coreutils gnu-tar fd
       ;;
     *)
       sudo apt update && sudo apt upgrade
-      brew install zsh tmux tig bat fzf
+      brew install zsh tmux tig bat fzf fd
 
       # enable zsh
       command -v zsh | sudo tee -a /etc/shells
