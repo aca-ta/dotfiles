72cab055666d2c6482e1adb75645e053fdffb975
aca-ta <seljydef@gmail.com>
2021-10-16 15:08:36 +0900

    update: nvm version.

--- setup.sh
+++ setup.sh
@@ -43,7 +43,7 @@ install_go(){
 
 install_nodejs() {
   if [[ ! -e ${HOME}/.nvm ]]; then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
   fi
   nvm install --lts
   nvm use --lts
