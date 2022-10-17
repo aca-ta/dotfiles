719a23386774504ed25834b28eeeb01022426db2
aca-ta <seljydef@gmail.com>
2022-10-17 09:58:09 +0900

    fix: create dir for symlinks.

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 
 set_symlinks() {
 
-  for file in $(find ./etc ! -type d | sed -e 's/^\.\/etc\///g')
-  do
-    rm -f ${HOME}/.${file}
-    echo "$(pwd)/etc/${file} to ~/.${file}"
-    ln -s $(pwd)/etc/${file} ~/.${file}
-  done
+    for dir in $(find ./etc/* -type d | sed -e 's/^\.\/etc\///g')
+    do
+        mkdir -p ${HOME}/.$dir
+    done
+
+    for file in $(find ./etc/* ! -type d | sed -e 's/^\.\/etc\///g')
+    do
+      rm -f ${HOME}/.${file}
+      echo "$(pwd)/etc/${file} to ~/.${file}"
+      ln -s $(pwd)/etc/${file} ~/.${file}
+    done
 }
 
 fetch_git_completions(){
