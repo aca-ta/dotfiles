afd1465f2605792c77eaf654aba9628110a0ca02
aca-ta <seljydef@gmail.com>
2024-10-06 11:26:29 +0900

    wilder fuzzy search

--- etc/config/nvim/lua/plugins/init.lua
+++ etc/config/nvim/lua/plugins/init.lua
@@ -17,8 +17,33 @@ return {
                 renderer = wilder.popupmenu_renderer(
                     {
                         pumblend = 20,
-                        highlighter = wilder.basic_highlighter()
+                        highlighter = { wilder.basic_highlighter(), wilder.lua_pcre2_highlighter() },
+                        highlights = {
+                            accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#f4468f' } }),
+                        },
+                    }
+                )
+            })
+            wilder.set_option('pipeline', {
+                wilder.branch(
+                    wilder.cmdline_pipeline({
+                        -- sets the language to use, 'vim' and 'python' are supported
+                        language = 'python',
+                        -- 0 turns off fuzzy matching
+                        -- 1 turns on fuzzy matching
+                        -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+                        fuzzy = 1,
+                    }),
+                    wilder.python_search_pipeline({
+                        -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+                        pattern = wilder.python_fuzzy_pattern(),
+                        -- omit to get results in the order they appear in the buffer
+                        sorter = wilder.python_difflib_sorter(),
+                        -- can be set to 're2' for performance, requires pyre2 to be installed
+                        -- see :h wilder#python_search() for more details
+                        engine = 're',
                     })
+                ),
             })
         end
     },
