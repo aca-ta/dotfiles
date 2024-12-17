26c5bc546db9670fe7d8c5812d713d7c723ca195
aca-ta <seljydef@gmail.com>
2024-12-17 09:10:20 +0900

    Revert "delete unused plugins."
    This reverts commit 8829b78a5319b8aee8f1b74fcbb936649c1c9ef5.

--- etc/config/nvim/lua/plugins/language.lua
+++ etc/config/nvim/lua/plugins/language.lua
@@ -1,3 +1,9 @@
+-- vim-markdown settings
+vim.g.vim_markdown_folding_disabled = 1
+vim.g.vim_markdown_conceal = 1
+vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_new_list_item_indent = 0
+
 -- vim-markdown-toc settings
 vim.g.vmt_fence_text = 'TOC START'
 vim.g.vmt_fence_closing_text = 'TOC END'
@@ -7,6 +13,11 @@ return {
     -- hive
     { 'autowitch/hive.vim' },
 
+    -- markdown
+    {
+        'plasticboy/vim-markdown',
+        dependencies = { 'godlygeek/tabular' },
+    },
     { 'mzlogin/vim-markdown-toc' },
     {
         'iamcco/markdown-preview.nvim',
@@ -56,9 +67,6 @@ return {
             "nvim-lua/plenary.nvim",
         },
         opts = {
-            ui = {
-                bullets = {},
-            },
             workspaces = {
                 {
                     name = "buf-parent",
@@ -71,6 +79,7 @@ return {
     },
 
     -- python
+    { 'psf/black',                            branch = 'stable' },
     {
         "linux-cultist/venv-selector.nvim",
         dependencies = {
