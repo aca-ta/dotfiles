20a41d8569190bfafb740987d2f64c365a40e50d
aca-ta <seljydef@gmail.com>
2025-04-14 13:40:24 +0900

    fix for poetry

--- etc/shrc.sh
+++ etc/shrc.sh
@@ -83,7 +83,6 @@ export PATH="$HOME/.local/bin:$PATH" #XXX
 if [ -e ~/.pyenv ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
-  export PATH="$HOME/.poetry/bin:$PATH"
   export PIPENV_VENV_IN_PROJECT=1
   pyenv() {
     unset -f pyenv
