27cba03b93b03aa985b4953ce2624d117adbf1e8
aca-ta <seljydef@gmail.com>
2025-06-01 09:43:32 +0900

    osc52

--- etc/config/nvim/init.lua
+++ etc/config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- Check for additional vimrc and source it if exists
 local additional_vimrc = vim.fn.expand("$HOME") .. "/.vim/additional_vimrc"
 if vim.fn.filereadable(additional_vimrc) == 1 then
-	vim.cmd("source " .. additional_vimrc)
+    vim.cmd("source " .. additional_vimrc)
 end
 -- nvim-tree.lua
 -- disable netrw at the very start of your init.lua
@@ -51,48 +51,48 @@ vim.opt.switchbuf:append({ "usetab", "newtab" })
 -------------------------------------------------
 -- Autocommands for filetypes
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = { "*.hql", "*.ddl" },
-	command = "setlocal filetype=hive expandtab tabstop=2 shiftwidth=2"
+    pattern = { "*.hql", "*.ddl" },
+    command = "setlocal filetype=hive expandtab tabstop=2 shiftwidth=2"
 })
 vim.api.nvim_create_autocmd("BufNewFile", {
-	pattern = { "*.py", "*.cpp", "*.sh" },
-	command = "0r $HOME/.vim/template/template." .. vim.fn.expand("%:e")
+    pattern = { "*.py", "*.cpp", "*.sh" },
+    command = "0r $HOME/.vim/template/template." .. vim.fn.expand("%:e")
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*Jenkinsfile*",
-	command = "setf groovy"
+    pattern = "*Jenkinsfile*",
+    command = "setf groovy"
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*qgs",
-	command = "setf xml"
+    pattern = "*qgs",
+    command = "setf xml"
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*plb",
-	command = "setf sql"
+    pattern = "*plb",
+    command = "setf sql"
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*plb",
-	command = "setf sql"
+    pattern = "*plb",
+    command = "setf sql"
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "Dockerfile.*",
-	command = "setf Dockerfile"
+    pattern = "Dockerfile.*",
+    command = "setf Dockerfile"
 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = { "*.yaml.liquid", "*.yml.liquid" },
-	command = "setf yaml"
+    pattern = { "*.yaml.liquid", "*.yml.liquid" },
+    command = "setf yaml"
 })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.conceallevel = 1
-	end,
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.conceallevel = 1
+    end,
 })
 
 if vim.fn.executable(vim.env.HOME .. '/.pyenv/versions/nvim/bin/python3') == 1 then
-	vim.g.python3_host_prog = vim.env.HOME .. '/.pyenv/versions/nvim/bin/python3'
+    vim.g.python3_host_prog = vim.env.HOME .. '/.pyenv/versions/nvim/bin/python3'
 else
-	vim.g.python3_host_prog = 'python3'
+    vim.g.python3_host_prog = 'python3'
 end
 
 
@@ -119,7 +119,7 @@ vim.g.loaded_matchit = 1
 
 -- format
 vim.api.nvim_create_user_command('Format', function()
-	vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+    vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
 end, {})
 
 -----------------------------------------
@@ -128,18 +128,18 @@ end, {})
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({ timeout = 1000 })
-	end
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({ timeout = 1000 })
+    end
 })
 
 -- clear highlight when insert mode.
 vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
-	group = vim.api.nvim_create_augroup("clear_highlight_yank", { clear = true }),
-	callback = function()
-		vim.api.nvim_command('highlight clear HighlightedyankRegion')
-	end
+    group = vim.api.nvim_create_augroup("clear_highlight_yank", { clear = true }),
+    callback = function()
+        vim.api.nvim_command('highlight clear HighlightedyankRegion')
+    end
 })
 
 require("lazy_nvim").setup(require("plugins"))
@@ -147,18 +147,41 @@ require("lazy_nvim").setup(require("plugins"))
 -- copilotChat
 local file = io.open(vim.env.HOME .. '/.config/github-copilot/apps.json', "r")
 if file then
-	file:close()
-	require("CopilotChat").setup {
-		debug = true, -- Enable debugging
-		-- See Configuration section for rest
-	}
+    file:close()
+    require("CopilotChat").setup {
+        debug = true, -- Enable debugging
+        -- See Configuration section for rest
+    }
 end
 
 -- 自動で更新を反映
 vim.api.nvim_create_autocmd("FocusGained", {
-	pattern = "*",
-	command = "checktime"
+    pattern = "*",
+    command = "checktime"
 })
+
+-- リモートの上 neovim からクリップボードにコピーする
+local use_osc52 = (os.getenv("USE_OSC52") == "true")
+if use_osc52 then
+    vim.opt.clipboard = ""
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        callback = function()
+            local regname = vim.v.event.regname
+            if regname == "x" then
+                local lines = vim.fn.getreg("x", 1, true)
+                local osc52_copy = require("vim.ui.clipboard.osc52").copy("+")
+                osc52_copy(lines)
+            end
+        end,
+    })
+    keymap("n", "<leader>y", '"xyy', { noremap = true, desc = "Yank line to x (clipboard)" })
+    keymap("v", "<leader>y", '"xy', { noremap = true, desc = "Yank selection to x (clipboard)" })
+    vim.notify("Clipboard mode: OSC52 only on register 'x' yank", vim.log.levels.INFO)
+else
+    vim.opt.clipboard = "unnamedplus"
+    vim.notify("Clipboard mode: unnamedplus", vim.log.levels.INFO)
+end
+
 
 vim.cmd [[
   syntax enable
