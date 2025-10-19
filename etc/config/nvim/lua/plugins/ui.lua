c1e5fe8157117778577af3d9daedc0e4355a31ef
aca-ta <seljydef@gmail.com>
2025-10-19 11:20:58 +0900

    fix opacity

--- etc/config/nvim/lua/plugins/ui.lua
+++ etc/config/nvim/lua/plugins/ui.lua
@@ -41,7 +41,7 @@ return {
         'sunjon/shade.nvim',
         config = function()
             require('shade').setup({
-                overlay_opacity = 70,
+                overlay_opacity = 75,
                 opacity_step = 1,
                 keys = {
                     toggle = '<Leader>s',
