12845854bd3bf971dd72711f2767d7f5b377ffe2
aca-ta <seljydef@gmail.com>
2025-11-05 13:17:03 +0900

    fix: toggleterm

--- etc/config/nvim/lua/plugins/ide.lua
+++ etc/config/nvim/lua/plugins/ide.lua
@@ -24,7 +24,14 @@ return {
         version = "*",
         config = function()
             require('toggleterm').setup({
-                direction = 'float'
+                direction = 'float',
+                size = function(term)
+                    if term.direction == "horizontal" then
+                        return vim.o.lines * 0.4
+                    elseif term.direction == "vertical" then
+                        return vim.o.columns * 0.4
+                    end
+                end
             })
         end
     },
