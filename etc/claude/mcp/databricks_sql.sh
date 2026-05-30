d2164deb825403cb2b98622947667f4288b48fdf
aca-ta <seljydef@gmail.com>
2026-05-30 18:30:07 +0900

    fix.

--- etc/claude/mcp/databricks_sql.sh
+++ etc/claude/mcp/databricks_sql.sh
@@ -6,10 +6,9 @@ if ! databricks auth token -p "$DATABRICKS_WORKSPACE_PROFILE" > /dev/null 2>&1; 
   databricks auth login -p "$DATABRICKS_WORKSPACE_PROFILE"
 fi
 DATABRICKS_WORKSPACE_HOSTNAME=`databricks auth describe -p $DATABRICKS_WORKSPACE_PROFILE -o json | jq -r '.details.host'`
-DATABRICKS_PERSONAL_ACCESS_TOKEN=`databricks auth token -p $DATABRICKS_WORKSPACE_PROFILE -o json | jq -r '.access_token'`
+export DATABRICKS_PERSONAL_ACCESS_TOKEN=`databricks auth token -p $DATABRICKS_WORKSPACE_PROFILE -o json | jq -r '.access_token'`
 
 # Launch MCP remote for Databricks SQL
 npx mcp-remote \
     $DATABRICKS_WORKSPACE_HOSTNAME/api/2.0/mcp/sql \
-    --header "Authorization: Bearer $DATABRICKS_PERSONAL_ACCESS_TOKEN"
-
+    --header 'Authorization: Bearer ${DATABRICKS_PERSONAL_ACCESS_TOKEN}'
